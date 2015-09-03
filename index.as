da58b65108d4a4d71eb733a197e35ef636fd4c49
Ryan Leckey <leckey.ryan@gmail.com>
2015-09-03 16:47:09 -0700

    Add keyword arguments and more error checking to the call op

--- index.as
+++ index.as
@@ -1,20 +1,11 @@
 // [ ] Type alias
 // [ ] Fold (run after analyze-usage but before analyze-module)
 
-// [-] Call {*}
-//    [x] Mutable parameters
-//    [x] Check for mutable parameters (to ensure captured arg is mutable)
-//    [x] Check for invalid keyword parameters
-//    [ ] Check for missing parameters {*}
-//    [ ] Check for too many parameters {*}
-//    [ ] Aggregate by-const-reference (struct, array, tuple, etc.) {*}
-//    [ ] Default arguments {*}
-//    [ ] Keyword arguments {*}
+// [-] Call
+//    [ ] Aggregate by-const-reference (struct, array, tuple, etc.)
+//    [ ] Default arguments
 
 // [ ] Varidac for extern "C" functions
-
-// [ ] Error: And/Or inside of if and while
-// [ ] Check break/continue when not in a loop
 
 // [ ] Intrinsic
 // NOTE: This is how the std will request an intrinsic (read: builtin) function
@@ -60,27 +51,3 @@
 // [ ] Implement (Trait methods)
 //    [ ] Parse
 //    [ ] Build/Expose/etc.
-
-
-// ------------
-
-//
-// trait Trignometry {
-//   def cos(self) -> Self;
-//   def sin(self) -> Self;
-//   def tan(self) -> Self;
-// }
-//
-// implement Trignometry for float32 {
-//   def cos(self) -> Self {
-//     return __cos_f32(self);
-//   }
-// }
-//
-// // NOTE: `-> type(..)` isn't needed as return types are auto-resolved but
-// //       just showing for completeness
-// def cos(value: Trignometry) -> type(value.cos()) {
-//   return value.cos();
-// }
-
-// ------------
--- src/pass/build/call.cpp
+++ src/pass/build/call.cpp
@@ -10,6 +10,27 @@
 namespace arrow {
 namespace pass {
 
+static Ref<code::TypeParameter> _param_for(
+  Ref<code::TypeFunction> type, ast::Argument& arg, unsigned arg_index
+) {
+  Ref<code::TypeParameter> param = nullptr;
+  if (arg.name.size() > 0) {
+    param = type->find_parameter(arg.name);
+    if (!param) {
+      Log::get().error(
+        arg.span,
+        "'%s' is an invalid keyword argument for this function",
+        arg.name.c_str());
+
+      return nullptr;
+    }
+  } else {
+    param = type->parameters.at(arg_index);
+  }
+
+  return param;
+}
+
 void Build::visit_call(ast::Call& x) {
   // Determine if we are a constructor (struct type) or a function
   if (x.operand.is<ast::Identifier>() ||
@@ -19,9 +40,6 @@ void Build::visit_call(ast::Call& x) {
       return do_struct_ctor(x, item);
     }
   }
-
-  // TODO(mehcode): Default
-  // TODO(mehcode): Keyword
 
   // Build the functor operand ..
   auto op = Build(_ctx, _scope).run_scalar(*x.operand);
@@ -33,9 +51,7 @@ void Build::visit_call(ast::Call& x) {
 
   // Resolve and build each argument (if any)
   // TODO(mehcode): Check for mis-matched types
-  // TODO(mehcode): Check for too many (or too little) arguments
-  std::vector<LLVMValueRef> arguments;
-  arguments.reserve(type->parameters.size());
+  std::vector<LLVMValueRef> arguments(type->parameters.size());
   unsigned arg_index = 0;
   for (auto& arg_node : x.arguments) {
     // Build the argument expression
@@ -44,23 +60,67 @@ void Build::visit_call(ast::Call& x) {
 
     LLVMValueRef arg_value = nullptr;
 
-    if (type->parameters.at(arg_index)->is_mutable) {
+    Ref<code::TypeParameter> param = _param_for(type, *arg_node, arg_index);
+    if (!param) return;
+
+    if (param->is_mutable) {
       // Use the address
       arg_value = arg->get_address(_ctx);
     } else {
       // Cast the argument to the appropriate type
-      arg = util::cast(
-        _ctx, arg, *arg_node->expression,
-        type->parameters.at(arg_index)->type, false);
-
+      arg = util::cast(_ctx, arg, *arg_node->expression, param->type, false);
       if (!arg) return;
 
       arg_value = arg->get_value(_ctx);
     }
 
-    // Add it to the positional arguments
-    arguments.push_back(arg_value);
-    arg_index += 1;
+    if (arg_node->name.size() > 0) {
+      auto param_index = 0;
+      auto match = false;
+      for (auto& param : type->parameters) {
+        if (param->keyword == arg_node->name) {
+          match = true;
+          break;
+        }
+
+        param_index += 1;
+      }
+
+      if (match) {
+        if (arguments[param_index] != nullptr) {
+          Log::get().error(
+            x.span,
+            "got multiple values for the argument: '%s'",
+            arg_node->name.c_str());
+
+          return;
+        }
+
+        // Add it to the keyword arguments
+        arguments[param_index] = arg_value;
+      } else {
+        return;
+      }
+    } else {
+      // Add it to the positional arguments
+      arguments[arg_index] = arg_value;
+      arg_index += 1;
+    }
+  }
+
+  // Check for missing parameters
+  unsigned missing_cnt = 0;
+  for (unsigned idx = 0; idx < arguments.size(); ++idx) {
+    if (arguments[idx] == nullptr) {
+      missing_cnt += 1;
+    }
+  }
+
+  if (missing_cnt > 0) {
+    Log::get().error(x.span, "expected %d arguments but received %d",
+      type->parameters.size(), type->parameters.size() - missing_cnt);
+
+    return;
   }
 
   // Build the call IR
@@ -79,7 +139,7 @@ void Build::do_struct_ctor(ast::Call& x, Ref<code::Structure> item) {
   // Segregate const and non-const elements
   // LLVM can create the initial tuple of const elements but subsequent
   // `insertvalue` instructions must be done to add non-constant values.
-  std::vector<LLVMValueRef> const_values;
+  std::vector<LLVMValueRef> const_values(type->members.size());
   std::vector<std::pair<unsigned, LLVMValueRef>> non_const_values;
 
   unsigned idx = 0;
@@ -88,22 +148,80 @@ void Build::do_struct_ctor(ast::Call& x, Ref<code::Structure> item) {
     auto arg = Build(_ctx, _scope).run_scalar(*arg_node->expression);
     if (!arg) return;
 
-    // Cast the argument to the appropriate type
-    arg = util::cast(
-      _ctx, arg, *arg_node->expression,
-      type->members.at(idx)->type, false);
+    Ref<code::TypeMember> mem = nullptr;
+    if (arg_node->name.size() > 0) {
+      mem = type->find_member(arg_node->name);
+      if (!mem) {
+        Log::get().error(
+          arg_node->span,
+          "type '%s' has no member '%s'",
+          type->name().c_str(),
+          arg_node->name.c_str());
 
+        return;
+      }
+    } else {
+      mem = type->members.at(idx);
+    }
+
+    // Cast the argument to the appropriate type
+    arg = util::cast(_ctx, arg, *arg_node->expression, mem->type, false);
     if (!arg) return;
+
+    auto mem_index = idx;
+    if (arg_node->name.size() > 0) {
+      auto match = false;
+      auto idx = 0;
+      for (auto& mem : type->members) {
+        if (mem->keyword == arg_node->name) {
+          match = true;
+          break;
+        }
+
+        idx += 1;
+      }
+
+      if (match) {
+        if (const_values[idx] != nullptr) {
+          Log::get().error(
+            x.span,
+            "got multiple values for the member: '%s'",
+            arg_node->name.c_str());
+
+          return;
+        }
+
+        // Add it to the keyword arguments
+        mem_index = idx;
+      } else {
+        return;
+      }
+    }
 
     auto value = arg->get_value(_ctx);
     if (LLVMIsConstant(value)) {
-      const_values.push_back(value);
+      const_values[mem_index] = value;
     } else {
-      const_values.push_back(LLVMGetUndef(LLVMTypeOf(value)));
-      non_const_values.push_back({idx, value});
+      const_values[mem_index] = LLVMGetUndef(LLVMTypeOf(value));
+      non_const_values.push_back({mem_index, value});
     }
 
     idx += 1;
+  }
+
+  // Check for missing args
+  unsigned missing_cnt = 0;
+  for (unsigned idx = 0; idx < const_values.size(); ++idx) {
+    if (const_values[idx] == nullptr) {
+      missing_cnt += 1;
+    }
+  }
+
+  if (missing_cnt > 0) {
+    Log::get().error(x.span, "expected %d members but received %d",
+      type->members.size(), type->members.size() - missing_cnt);
+
+    return;
   }
 
   // Create the initial const struct
